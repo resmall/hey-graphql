0a391abfbea1afb472694394d885e54b19cb3d88
Tiago R. Lammers <trlammers@gmail.com>
2018-03-29 17:27:51 -0300

    Object Type mapping

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
 	type Order {
-	  id: ID
-	  code: String
+		prices: [Int!]
+		getPrice: Int!
+		applyDiscount(discount_value: Int!): Int!
 	}
 	
 	type Query {
-	  getOrder(id: Int!): Order
+	  getOrder(prices: [Int]): Order
 	}
 `);
 
 
 var root = { 
-	getOrder: ({id}) => {
-		let arr = [{'id': 123, 'code': 'ASADD'}, {'id': 321, 'code': 'D#D4'}];
-		for (let i = 0; i <= arr.length; i++) {
-			if (arr[i].id == id) {
-				return arr[i];
-			}
-		}
+	getOrder: ({prices}) => {
+		// let arr = [{'id': 123, 'code': 'ASADD'}, {'id': 321, 'code': 'D#D4'}];
+		// for (let i = 0; i <= arr.length; i++) {
+		// 	if (arr[i].id == id) {
+		// 		return arr[i];
+		// 	}
+		// }
+		return new Order(prices);
 	}
 };
+
+class Order {
+  constructor(prices) {
+    this.prices = prices;
+  }
+
+  getPrice() {
+		let price = 0;
+    for (let i = 0; i < this.prices.length; i++) {
+			price += this.prices[i];
+		}
+		return price;
+  }
+
+  applyDiscount({discount_value}) {
+    return this.getPrice() - discount_value;
+  }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
